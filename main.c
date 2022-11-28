b0aabc74cb973c6157b73fe1248b1d1ffc22d073
Samy De Smedt <sde-smed@fu-r4-p7.s19.be>
2022-11-28 14:21:57 +0100

    add key_hook

--- main.c
+++ main.c
@@ -3,22 +3,44 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: samy <samy@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: sde-smed <sde-smed@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/26 21:43:21 by samy              #+#    #+#             */
-/*   Updated: 2022/11/27 22:28:10 by samy             ###   ########.fr       */
+/*   Updated: 2022/11/28 14:19:09 by sde-smed         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 // cc -I /usr/local/include main.c -L ./minilibx -lmlx -framework OpenGL -framework AppKit
 
 #include "so_long.h"
-#include <unistd.h>
+#include <stdio.h>
 
 int	deal_mouse(int button, int x, int y, void *param)
 {
 	t_game	*game = (t_game *)param;
+
+	mlx_clear_window(game->mlx, game->window);
 	mlx_pixel_put(game->mlx, game->window, x, y, 0xFFFFFF);
+	mlx_string_put(game->mlx, game->window, x, y, 0xFFFFFF,"coucou");
+	return (0);
+}
+
+int	deal_keys(int keycode, void *param)
+{
+	t_game	*game = (t_game *)param;
+	char	str[42];
+
+	sprintf(str, "%d", keycode);
+	if (keycode == 53){
+		mlx_clear_window(game->mlx, game->window);
+		mlx_string_put(game->mlx, game->window, 400, 300, 0xFFFFFF, "bye");
+		mlx_destroy_window(game->mlx, game->window);
+	}
+	else
+	{
+		mlx_clear_window(game->mlx, game->window);
+		mlx_string_put(game->mlx, game->window, 400, 300, 0xFFFFFF, str);
+	}
 	return (0);
 }
 
@@ -27,7 +49,9 @@ int	main(void)
 	t_game	game;
 
 	game.mlx = mlx_init();
-	game.window = mlx_new_window(game.mlx, 1920, 1080, "hello");
+	game.window = mlx_new_window(game.mlx, 800, 600, "hello");
 	mlx_mouse_hook(game.window, deal_mouse, &game);
+	mlx_key_hook(game.window, deal_keys, &game);
 	mlx_loop(game.mlx);
+	return (0);
 }
--- so_long.h
+++ so_long.h
@@ -3,17 +3,17 @@
 /*                                                        :::      ::::::::   */
 /*   so_long.h                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: samy <samy@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: sde-smed <sde-smed@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/27 21:53:32 by samy              #+#    #+#             */
-/*   Updated: 2022/11/27 22:13:44 by samy             ###   ########.fr       */
+/*   Updated: 2022/11/28 13:36:08 by sde-smed         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SO_LONG_H
 # define SO_LONG_H
 
-# include "mlx.h"
+# include <mlx.h>
 
 typedef struct s_game
 {
